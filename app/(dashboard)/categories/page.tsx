06bcdf38253ccc28e8a37211b64cc53bbcef0600
JesusVA760 <tonio4340406@gmail.com>
2025-01-11 23:58:54 -0800

    Cateogory/Categories Model/Schema, Route/Page, Handlers, & UI

---
+++ app/(dashboard)/categories/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Loader2, Plus } from "lucide-react";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { columns } from "./columns";
+import { Skeleton } from "@/components/ui/skeleton";
+import { DataTable } from "@/components/ui/data-table";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
+const CategoryPage = () => {
+  const newAccount = useNewAccount();
+  const deleteAccounts = useBulkDeleteAccounts();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending;
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-2-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-non drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+  return (
+    <div className="max-2-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-non drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">
+            Categories Page
+          </CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids });
+            }}
+            filterKey="name"
+            columns={columns}
+            data={accounts}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default CategoryPage;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,13 +3,16 @@ import { Hono } from "hono";
 // import { zValidator } from "@hono/zod-validator";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
+import categories from "./categories";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 app.route("/accounts", accounts);
 
-const routes = app.route("/accounts", accounts);
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories);
 
 export const GET = handle(app);
 export const POST = handle(app);
