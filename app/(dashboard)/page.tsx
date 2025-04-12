51fa0af7b7a88ae7eb3f2a289624eb3c9d26f06a
JesusVA760 <tonio4340406@gmail.com>
2025-04-12 13:00:38 -0700

    Dashboard Icons

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,9 @@
-"use client";
+import { DataGrid } from "@/components/data-grid";
 
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-// import Image from "next/image";
-
-export default function Home() {
-  const { onOpen } = useNewAccount();
+export default function DashbaordPage() {
   return (
-    <div onClick={onOpen}>
-      <Button>Add an account</Button>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
